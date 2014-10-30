b3d0d167bed0a8035aa6a69409df4e068c62834e
maxbarry <maxbarrymail@gmail.com>
2014-10-30 00:31:47 +0000

    Fetching all feeds nice and good

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,7 @@ var mbry = {
     accounts: {
         medium: "@ev",
         twitter: "",
-        twitter_widget: "420890443182649345"
-    },
-    social: {
-        medium: {},
-        tweets: {},
-        github: {}
+        twitter_widget: "420890443182649345",
+        github: "max-barry",
     }
 };
