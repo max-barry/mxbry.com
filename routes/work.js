e470252d381c0365c6bfebce58a35435cb4e9b87
maxbarry <maxbarrymail@gmail.com>
2015-01-13 23:22:49 +0000

    404 on missing mixdown

--- routes/work.js
+++ routes/work.js
@@ -3,16 +3,21 @@ exports.index = function(req, res) {
 };
 
 exports.slug = function(req, res) {
-    var slug = req.params.slug,
-        works = require("../public/data/projects.json").projects,
-        _ = require("lodash"),
+    var _ = require("lodash"),
         fs = require('fs');
 
-    var work = _.find(works, {"slug": slug}),
-        mdown_file = require.resolve("../public/data/" + slug + ".mdown");
-        mdown = fs.readFileSync(mdown_file, "utf8");
+    var slug = req.params.slug,
+        works = require("../public/data/projects.json").projects,
+        work = _.find(works, {"slug": slug});
 
-    if (!!work) {
+    try {
+        var mdown_file = require.resolve("../public/data/" + slug + ".mdown"),
+            mdown = fs.readFileSync(mdown_file, "utf8");
+    } catch(e) {
+        var mdown = null;
+    }   
+
+    if (!!work && !!mdown) {
         res.render("pages/project.html", {
             project: work,
             body: mdown
