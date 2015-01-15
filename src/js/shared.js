2dd180b084febc12e0bf4e2ccc56207e52a2380c
maxbarry <maxbarrymail@gmail.com>
2015-01-15 00:09:39 +0000

    New stylings on work page

--- src/js/shared.js
+++ src/js/shared.js
@@ -2,10 +2,4 @@ $(function() {
     $(".scale-takeover-trigger").on("click", function(){
         $(this).siblings("nav").toggleClass("active");
     });
-
-    if ($("[data-random-project]").length) {
-        mb.utils.generate_random_project();
-    } else {
-        console.log("Not on this page.");
-    }
 });
