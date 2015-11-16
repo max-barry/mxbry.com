dd78cb40887794e8a4f6c69e5b6565469f054cab
Max Barry <max.barry@havasww.com>
2015-11-16 10:55:51 -0500

    Updated page meta

--- data/meta.js
+++ data/meta.js
@@ -1,5 +1,5 @@
-var title = "Max Barry - A Digital Dude's Portfolio | mxbry.com",
-    description = "Max Barry makes stuff on the internet. He's way too in to site performance, statistical analysis, data research, design and UX bits, Python and Django, Javascript, Node junk like Grunt, CSS3, and HTML5. It's kind of weird.",
+var title = "mxbry.com | Max Barry - A Digital Dude's Portfolio",
+    description = "mxbry.com is the portfolio of Max Barry, who makes stuff on the internet. He's in to site performance, design and UX, Python and Django, Javascript, Node junk like Grunt and Express, CSS3, and HTML5",
     keywords = "website,web,developer,digital,python,django,node,grunt,yeoman,london,uk,ux,design,internet",
     site_img = "http://www.mxbry.com/static/images/logos/mxbry.jpg";
 
