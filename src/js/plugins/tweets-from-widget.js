0363c008b0782d06cb7d0e18c4e5ee59550768db
maxbarry <maxbarrymail@gmail.com>
2016-05-02 20:47:30 +0100

    Fixed twitter integration

--- src/js/plugins/tweets-from-widget.js
+++ src/js/plugins/tweets-from-widget.js
@@ -34,25 +34,29 @@ Fetches and creates objects representing Tweets within a Twitter widget.
         }
 
         function _build_tweet_objects(resp) {
+
             if (resp.headers.status != 200) {
                 throw ("Twitter service returned error");
             }
 
-            var tweet_elements = $(resp.body).find(".tweet"),
-                tmp_element, tmp_tweet,
+            var tweet_elements = $(resp.body).find(".timeline-TweetList-tweet"),
+                tmp_element, tmp_tweet, tmp_scrnname, tmp_id,
                 extra_details, author_details;
 
             for (var i = tweet_elements.length - 1; i >= 0; i--) {
                 tmp_element = $(tweet_elements[i]);
                 tmp_tweet = {};
                 
-                tmp_tweet.body = tmp_element.find(".e-entry-title").text();
-                tmp_tweet.pub_date = tmp_element.find("a.permalink").data("datetime");
+                tmp_tweet.body = tmp_element.find(".timeline-Tweet-text").text();
+                tmp_tweet.pub_date = tmp_element.find("time").attr("datetime");
+
+                tmp_scrnname = tmp_element.find(".TweetAuthor-screenName").text();
+                tmp_id = tmp_element.find(".timeline-Tweet").data("tweet-id");
 
                 if (!settings.slim) {
                     extra_details = {
-                        id: toString(tmp_element.data("tweet-id")),
-                        url: "https://twitter.com/" + tmp_element.find(".p-author .p-nickname").text() + "/status/" + tmp_element.data("tweet-id")
+                        id: tmp_id,
+                        url: "https://twitter.com/" + tmp_scrnname.replace("@", "").toString() + "/status/" + tmp_id
                     };
 
                     if (tmp_element.find(".retweet-credit").length) {
@@ -63,10 +67,10 @@ Fetches and creates objects representing Tweets within a Twitter widget.
                 if (!settings.slim && settings.author) {
                     author_details = {
                         author: {
-                            username: tmp_element.find(".p-author .p-nickname").text(),
-                            name: tmp_element.find(".p-author .p-name").text(),
-                            profile_image: tmp_element.find(".u-photo.avatar").attr("src"),
-                            retina_profile_image: tmp_element.find(".u-photo.avatar").attr("data-src-2x")
+                            username: tmp_scrnname,
+                            name: tmp_element.find(".TweetAuthor-name").text(),
+                            profile_image: tmp_element.find(".TweetAuthor-avatar img").attr("data-src-1x"),
+                            retina_profile_image: tmp_element.find(".TweetAuthor-avatar img").attr("data-src-2x")
                         }
                     };
                 }
