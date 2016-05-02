28e95ecb13db32fbf9ae552d36c9210e93facec9
maxbarry <maxbarrymail@gmail.com>
2016-05-02 22:42:58 +0100

    Sitemamp modifications

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -1,28 +1,37 @@
 var sm = require("sitemap");
+var projectFetcher = require("./get_projects.js");
 
 exports.index = function(req, res) {
-    var projects = require("../public/data/projects.json").projects,
-        urls = [
+
+
+    projectFetcher.retrieve(function(err, projects){
+
+        var urls = [
             { url: "/", changefreq: "weekly", priority: 0.9 },
             { url: "/work/", changefreq: "weekly", priority: 0.7 }
         ];
-    
-    for (var i = 0; i < projects.length; i++) {
-        urls.push({
-            url: "/work/" + projects[i].slug,
-            changefreq: "monthly",
-            priority: 0.8
-        })
-    };
+        
+        for (var i = 0; i < projects.length; i++) {
+            if (!projects[i].external) {
+                urls.push({
+                    url: "/work/" + projects[i].slug,
+                    changefreq: "monthly",
+                    priority: 0.8
+                })
+            }
+        };
 
-    sitemap = sm.createSitemap({
-        hostname: req.protocol + "://" + req.headers.host,
-        cacheTime: 600000,
-        urls: urls
+        sitemap = sm.createSitemap({
+            hostname: req.protocol + "://" + req.headers.host,
+            cacheTime: 600000,
+            urls: urls
+        });
+        
+        sitemap.toXML(function(xml){
+            res.header("Content-Type", "application/xml");
+            res.send(xml);
+        })
+
     });
-    
-    sitemap.toXML(function(xml){
-        res.header("Content-Type", "application/xml");
-        res.send(xml);
-    })
+
 };
