95043de304d34f65a2e031fa50b83783bd64a2f9
maxbarry <maxbarrymail@gmail.com>
2016-05-31 16:41:55 +0100

    Font crisis

--- src/js/reactComponents/layouts/react.Navigation.jsx
+++ src/js/reactComponents/layouts/react.Navigation.jsx
@@ -19,16 +19,18 @@ export class Navigation extends React.Component {
                 <Main>
                     { this.props.children }
                 </Main>
-                <footer className='footer'>
-                    <div className='footer__main'>
-                        <p>Discover <Link activeClassName='active' to='/work'>work I've done</Link> for clients and open source projects, as well as some of the <Link activeClassName='active' to='/where-max-is-active'>places around the internet</Link> that I am active.</p>
-                        <p>Contact me on <a target='blank' href='https://twitter.com/mxbrry'>Twitter</a>, <a target='blank' href='https://github.com/max-barry'>Github</a>, <a target='blank' href='mailto:max@mxbry.com'>Email</a>, <a target='blank' href='https://words.mxbry.com/'>Medium</a> or <a target='blank' href='https://uk.linkedin.com/in/maxbarry'>LinkedIn</a></p>
-                    </div>
-                    <div className='footer__foot'>
-                        <h5>mxbry.com</h5>
-                    </div>
-                </footer>
             </div>
         );
     }
 }
+
+
+// <footer className='footer'>
+//     <div className='footer__main'>
+//         <p>Discover <Link activeClassName='active' to='/work'>work I've done</Link> for clients and open source projects, as well as some of the <Link activeClassName='active' to='/where-max-is-active'>places around the internet</Link> that I am active.</p>
+//         <p>Contact me on <a target='blank' href='https://twitter.com/mxbrry'>Twitter</a>, <a target='blank' href='https://github.com/max-barry'>Github</a>, <a target='blank' href='mailto:max@mxbry.com'>Email</a>, <a target='blank' href='https://words.mxbry.com/'>Medium</a> or <a target='blank' href='https://uk.linkedin.com/in/maxbarry'>LinkedIn</a></p>
+//     </div>
+//     <div className='footer__foot'>
+//         <h5>mxbry.com</h5>
+//     </div>
+// </footer>
