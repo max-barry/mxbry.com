9ecc66ea6f7482c4202b73cd2b6fb991fb901fdb
Max Barry <max@mxbry.com>
2022-03-02 10:07:44 +0100

    ADD: Contact form

---
+++ src/components/contact/contact.component.tsx
@@ -0,0 +1,43 @@
+import { ReactComponent as Medium } from "../../statics/icons/contact-medium.svg";
+import { ReactComponent as Github } from "../../statics/icons/contact-github.svg";
+import { ReactComponent as LinkedIn } from "../../statics/icons/contact-linkedin.svg";
+import { Frame, Major, Minor } from "./contact.styles";
+
+interface Props {}
+
+export const Contact: React.FC<Props> = () => (
+  <Frame>
+    <Major>Who</Major>
+    <Minor>
+      This the portfolio of Max Barry, experience and prouct engineer.
+    </Minor>
+    <Major>What</Major>
+    <Minor>
+      I often work with new or early-stage businesses; talking about product
+      direction and creative development; early-stage growth ideas; growing a
+      business through new hires; and technical advice and engineering
+      implementation.
+    </Minor>
+    <Major>Email</Major>
+    <Minor>max (at) mxbry.com</Minor>
+    <Major>Further Contact</Major>
+    <Minor>
+      Get in touch by email, or connect on{" "}
+      <a
+        href="https://www.linkedin.com/in/maxbarry"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedIn /> LinkedIn.
+      </a>{" "}
+      My Github is{" "}
+      <a href="https://github.com/max-barry" target="_blank" rel="noreferrer">
+        <Github /> max-barry.
+      </a>{" "}
+      I semi-frequently{" "}
+      <a href="https://maxbarry.medium.com/" target="_blank" rel="noreferrer">
+        <Medium /> write on Medium.
+      </a>
+    </Minor>
+  </Frame>
+);
---
+++ src/components/contact/contact.styles.ts
@@ -0,0 +1,101 @@
+import styled from "@emotion/styled";
+
+import { container } from "../../helpers/style-shortcuts.helpers";
+import { rhythm } from "../../styles/typography.styles";
+
+export const Major = styled.h1`
+  margin-bottom: 0;
+`;
+
+export const Minor = styled.h3`
+  margin-bottom: 0;
+  font-weight: 300;
+  padding-top: ${rhythm(1 / 4)};
+
+  a {
+    --pad: ${rhythm(1 / 4)};
+
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    background-color: var(--colors-neutral-10);
+    border-radius: 3px;
+    cursor: pointer;
+    padding: 0 var(--pad);
+    gap: var(--pad);
+    font-weight: 400;
+    overflow: hidden;
+  }
+
+  a svg {
+    top: 1px;
+    position: relative;
+    width: 1em;
+  }
+
+  a::before,
+  a::after {
+    content: "";
+    width: 1em;
+    height: 1em;
+    position: absolute;
+    left: var(--pad);
+    top: 50%;
+    opacity: 0;
+    transform: translate3d(calc(-100% - var(--pad)), -50%, 0);
+  }
+
+  a::before {
+    border-radius: 4px;
+    background-color: var(--colors-primary-110);
+  }
+
+  a::after {
+    content: "go";
+    font-size: 0.4em;
+    width: auto;
+    color: white;
+    line-height: 1;
+    padding-left: 0.6em;
+    font-weight: 500;
+  }
+
+  a svg,
+  a::before,
+  a::after {
+    will-change: transform, opacity;
+    transition: transform 140ms ease-in, opacity 100ms ease-in;
+  }
+
+  &:hover,
+  &:focus {
+    svg,
+    &::before,
+    &::after {
+      transition: transform 250ms ease-out, opacity 80ms ease-out;
+    }
+  }
+
+  a:hover,
+  a:focus {
+    svg {
+      transform: translateX(100%);
+      opacity: 0;
+    }
+
+    &::before,
+    &::after {
+      transform: translate3d(0%, -50%, 0);
+      opacity: 1;
+    }
+  }
+`;
+
+export const Frame = styled.div`
+  display: grid;
+  padding-bottom: ${rhythm(2)};
+  grid-template-columns: min-content 1fr;
+  grid-gap: ${rhythm(3 / 2)} ${rhythm(2)};
+  align-items: start;
+  ${container}
+`;
